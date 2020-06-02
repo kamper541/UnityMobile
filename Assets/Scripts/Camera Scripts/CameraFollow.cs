8ea914f7af648b873b19b3a753eb651300eb8b88
kamper541 <kamper543@gmail.com>
2020-06-02 22:06:35 +0700

    no.1

---
+++ Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour{
+
+    public float moveSmoothing = 10f;
+    public float rotationSmoothing = 15f;
+
+    private Transform target ;
+    private Vector3 targetForward;
+
+    void Awake() {
+        target = GameObject.FindWithTag(Tags.PLAYER_TAG).transform;
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        targetForward = transform.forward;
+        targetForward.y = 0f;
+        Snap();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        FollowPlayer();
+    }
+
+    void Snap(){
+        if(target != null){
+            transform.position = target.position;
+
+        }
+        Vector3 forward = targetForward;
+        forward.y = transform.forward.y;
+        transform.forward = forward;
+    }
+
+    void FollowPlayer(){
+        if(target != null){
+            transform.position = 
+                Vector3.Lerp(transform.position, target.position, Time.deltaTime * moveSmoothing);
+        }
+
+        Vector3 forward = transform.forward;
+        forward.y = 0f;
+        forward = Vector3.Slerp(forward, forward, Time.deltaTime * rotationSmoothing);
+        forward.y = transform.forward.y;
+        transform.forward = forward;
+
+    }
+}
---
+++ Assets/Scripts/Platform Scripts/MovingPlatform.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+        [SerializeField]
+        private Transform movePoint;
+
+        private Vector3 startPosition;
+
+        [SerializeField]
+        private float smoothMovement = 0.3f;
+        private float initailMovement;
+
+        private bool smoothMovementHalfed;
+
+        private bool can_Move;
+        private bool move_to_Initial;
+
+        [SerializeField]
+        private float halfDistance = 15f;
+
+        [SerializeField]
+        private bool activateMovementInStart;
+        
+        [SerializeField]
+        private float timer = 1f;
+    // Start is called before the first frame update
+    void Awake() {
+        startPosition = transform.position;
+        initailMovement = smoothMovement;
+    }
+    void Start(){
+        if(activateMovementInStart){
+            Invoke("ActivateMovement",timer);
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        MovePlatform();
+    }
+
+    void MovePlatform(){
+        if(can_Move){
+            transform.position = Vector3.MoveTowards(transform.position,
+            movePoint.position, smoothMovement);
+
+            if(Vector3.Distance(transform.position, movePoint.position) <= halfDistance){
+                if(!smoothMovementHalfed){
+
+                    smoothMovement /= 2f;
+                    smoothMovementHalfed = true;
+                }
+            }
+
+            if(Vector3.Distance(transform.position, movePoint.position) == 0f){
+                can_Move = false;
+
+                if(smoothMovementHalfed){
+                    smoothMovement = initailMovement;
+                    smoothMovementHalfed = false;
+                }
+
+
+            }
+        }
+    }
+
+    public void ActivateMovement(){
+        can_Move = true;
+    }
+}
